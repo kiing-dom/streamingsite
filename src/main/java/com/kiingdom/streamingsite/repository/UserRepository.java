67e7e8a0dbb929815163bdda7027c5b50aa8796b
Dominion Gbadamosi <worshipgbadamosi@live.com>
2024-07-25 18:17:11 +0100

    added ServiceImpl file and implemented all service methods. | changed get methods to use Optional type

--- src/main/java/com/kiingdom/streamingsite/repository/UserRepository.java
+++ src/main/java/com/kiingdom/streamingsite/repository/UserRepository.java
@@ -1,10 +1,11 @@
 package com.kiingdom.streamingsite.repository;
+import com.kiingdom.streamingsite.model.User;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import com.kiingdom.streamingsite.model.User;
+import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, Long> {
-    User findByUsername(String username);
-    User findByEmail(String email);
+    Optional<User> findByUsername(String username);
+    Optional<User> findByEmail(String email);
 }
