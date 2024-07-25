2afec3ee75c314d6c3a363dd232202b25927932f
Dominion Gbadamosi <worshipgbadamosi@live.com>
2024-07-25 18:17:11 +0100

    added ServiceImpl file and implemented all service methods. | changed get methods to use Optional type

--- src/main/java/com/kiingdom/streamingsite/service/UserService.java
+++ src/main/java/com/kiingdom/streamingsite/service/UserService.java
@@ -2,13 +2,14 @@ package com.kiingdom.streamingsite.service;
 import com.kiingdom.streamingsite.model.User;
 
 import java.util.List;
+import java.util.Optional;
 
 
 public interface UserService {
     User saveUser(User user);
-    User getUserById(Long id);
-    User getUserByEmail(String email);
-    User getUserByUsername(String username);
+    Optional<User> getUserById(Long id);
+    Optional<User> getUserByEmail(String email);
+    Optional<User> getUserByUsername(String username);
     List<User> getAllUsers();
     void deleteUser(Long id);
 }
