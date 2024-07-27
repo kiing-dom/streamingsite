8c5583dc474782a2d0861e99982145bd425a3ece
Dominion Gbadamosi <worshipgbadamosi@live.com>
2024-07-27 20:49:02 +0100

    changed project backend build directory

---
+++ backend/src/main/java/com/kiingdom/streamingsite/StreamingsiteApplication.java
@@ -0,0 +1,13 @@
+package com.kiingdom.streamingsite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StreamingsiteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StreamingsiteApplication.class, args);
+	}
+
+}
---
+++ backend/src/main/java/com/kiingdom/streamingsite/controller/UserController.java
@@ -0,0 +1,65 @@
+package com.kiingdom.streamingsite.controller;
+
+import java.util.List;
+import java.util.Optional;
+
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.kiingdom.streamingsite.model.User;
+import com.kiingdom.streamingsite.service.UserService;
+
+@RestController
+@RequestMapping("/api/users")
+public class UserController {
+    
+    private final UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+        @PostMapping
+        public ResponseEntity<User> createUser(@RequestBody User user) {
+            User createdUser = userService.saveUser(user);
+            return new ResponseEntity<>(createdUser, HttpStatus.CREATED);
+        }
+
+        @GetMapping("/{id}")
+        public ResponseEntity<User> getUserById(@PathVariable Long id) {
+            Optional<User> user = userService.getUserById(id);
+            return user.map(ResponseEntity::ok).orElseGet(() -> new ResponseEntity<>(HttpStatus.NOT_FOUND));
+        }
+
+        @GetMapping("/username/{username}")
+        public ResponseEntity<User> getUserByUsername(@PathVariable String username) {
+            Optional<User> user = userService.getUserByUsername(username);
+            return user.map(ResponseEntity::ok).orElseGet(() -> new ResponseEntity<>(HttpStatus.NOT_FOUND));
+        }
+
+        @GetMapping("/email/{email}")
+        public ResponseEntity<User> getUserByEmail(@PathVariable String email) {
+            Optional<User> user = userService.getUserByEmail(email);
+            return user.map(ResponseEntity::ok).orElseGet(() -> new ResponseEntity<>(HttpStatus.NOT_FOUND));
+        }
+
+        @GetMapping
+        public ResponseEntity<List<User>> getAllUsers() {
+            List<User> users = userService.getAllUsers();
+            return new ResponseEntity<>(users, HttpStatus.OK);
+        }
+
+        @DeleteMapping("/{id}")
+        public ResponseEntity<Void> deleteUser(@PathVariable Long id) {
+            userService.deleteUser(id);
+            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+        }
+}
