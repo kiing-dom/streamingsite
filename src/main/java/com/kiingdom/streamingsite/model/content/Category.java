0b3cc5d0cb4386e776d6f1ae59a60f83bd05ddbb
Dominion Gbadamosi <worshipgbadamosi@live.com>
2024-08-14 23:16:58 +0100

    added getters and setters to category to fix error

--- src/main/java/com/kiingdom/streamingsite/model/content/Category.java
+++ src/main/java/com/kiingdom/streamingsite/model/content/Category.java
@@ -23,4 +23,28 @@ public class Category {
     @Size(max = 200)
     private String description;
     
+    public Long getId() {
+        return this.id;
+    }
+    
+    public void setId(Long id) {
+        this.id = id;
+    }
+    
+    public String getName() {
+        return this.name;
+    }
+    
+    public void setName(String name) {
+        this.name = name;
+    }
+    
+    public String getDescription() {
+        return this.description;
+    }
+    
+    public void setDescription(String description) {
+        this.description = description;
+    }
 }
+    
